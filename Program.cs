e78fd650489c31b0a64dbf44e66e0f85f7f06fec
GuilhermePaini <guihdpaini@gmail.com>
2025-11-29 14:14:44 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿namespace AtividadeLabIV;
+
+public class Pedido
+{
+    public Guid Id { get; private set; }
+    public string Cliente { get; private set; } = default!;
+    public List<string> Produtos { get; private set; } = [];
+    public double ValorTotal { get; private set; }
+
+    private Pedido()
+    {}
+
+    public static Pedido CreateInstance(
+        string cliente,
+        List<string> produtos,
+        double valorTotal)
+    {
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Cliente = cliente,
+            Produtos = produtos,
+            ValorTotal = valorTotal
+        };
+    }
+
+    public void Update(string cliente, List<string> produtos, double valorTotal)
+    {
+        Cliente = cliente;
+        Produtos = produtos;
+        ValorTotal = valorTotal;
+    }
+}
+
+public class LojaVirtual
+{
+    private readonly List<Pedido> Pedidos = []; 
+    
+    public void CadastrarPedido(Pedido pedido)
+    {
+        Pedidos.Add(pedido);
+    }
+
+    public List<Pedido> ListarPedidos() => Pedidos;
+
+    public void AlterarPedido(Guid id, Pedido pedido)
+    {
+        var pedidoParaAtualizar = Pedidos.FirstOrDefault(x => x.Id == id) 
+            ?? throw new ArgumentOutOfRangeException(nameof(id));
+
+        pedidoParaAtualizar.Update(
+            pedido.Cliente,
+            pedido.Produtos,
+            pedido.ValorTotal);
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var loja = new LojaVirtual();
+
+        Console.WriteLine("Bomdia");
+    }
+}
